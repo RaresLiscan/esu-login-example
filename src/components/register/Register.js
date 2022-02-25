d1bc38fdbf737c01c3f934054939da78bdc489d9
Radu Herzal <rherzal@gmail.com>
2022-02-25 14:47:27 +0200

    added password checking

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './styles.css';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 import { Link } from "react-router-dom";
+import { isValidDateValue } from "@testing-library/user-event/dist/utils";
 
 
 const Register = () => {
@@ -12,13 +13,20 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirm, setConfirm] = useState('');
+
+    const [invalid, setInvalid] = useState(false)
+    const [error, setError] = useState('')
+
     const navigate = useNavigate();
     const authentication = getAuth();
 
 
     const handleRegister = async () => {
+        if (password.length < 6) {
+            alert("Parola nu e destul de lunga pentru firebase")
+        }
         if (password.localeCompare(confirm) === 0) {
-            await createUserWithEmailAndPassword(authentication, email, password)  
+            await createUserWithEmailAndPassword(authentication, email, password)
                 .then(res => {
                     console.log(res);
                     navigate('/');
@@ -29,17 +37,37 @@ const Register = () => {
         }
     }
 
+    const validate = () => {
+        if (password.length < 6) {
+            setInvalid(true)
+            setError("Parola e prea scurta")
+        } else {
+            if (password.localeCompare(confirm) != 0) {
+                setInvalid(true)
+                setError("Parolele nu coincid")
+            } else {
+                setInvalid(false)
+                setError("")
+            }
+        }
+        
+    }
+
+    useEffect(() => {
+        validate();
+    },[password, confirm])
+
     return (
         <div className="container">
-            <h1 style={{textAlign: 'center'}}>Register</h1>
+            <h1 style={{ textAlign: 'center' }}>Register</h1>
             <div className="form-field">
                 <TextField id="registerEmail" label="Email" type={"email"} variant="standard" value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             <div className="form-field">
-                <TextField id="registerPass" label="Password" type={"password"} variant="standard" value={password} onChange={e => setPassword(e.target.value)} />
+                <TextField error={invalid} helperText={error} id="registerPass" label="Password" type={"password"} variant="standard" value={password} onChange={e => setPassword(e.target.value)} />
             </div>
             <div className="form-field">
-                <TextField id="registerConfirm" label="Confirm Password" type={"password"} variant="standard" value={confirm} onChange={e => setConfirm(e.target.value)} />
+                <TextField error={invalid} helperText={error} id="registerConfirm" label="Confirm Password" type={"password"} variant="standard" value={confirm} onChange={e => setConfirm(e.target.value)} />
             </div>
             <div>
                 <Button className="login-button" variant="contained" onClick={handleRegister}>Register</Button>
